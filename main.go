cffc09f79fa0c6ccb418d917a11347eef679cdd4
antoniaklja <antoniaklja@gmail.com>
2017-08-29 14:19:38 +0200

    #4 Initial version of cli commands

--- main.go
+++ main.go
@@ -11,12 +11,47 @@ func main() {
   app.Name = "autograph"
   app.Version = "0.1"
 
-  app.Action = func(c *cli.Context) error {
-    fmt.Printf("Autograph")
-    return nil
+  app.Commands = []cli.Command{
+    {
+      Name:    "generate",
+      Aliases: []string{"g"},
+      Usage:   "generate a key and certificate pair",
+      Action:  func(c *cli.Context) error {
+        fmt.Println("generate is not implemented yet")
+        return nil
+      },
+    },
+    {
+      Name:    "sign",
+      Aliases: []string{"s"},
+      Usage:   "sign a manifest",
+      Action:  func(c *cli.Context) error {
+        fmt.Println("sign is not implemented yet")
+        return nil
+      },
+    },
+    {
+      Name:        "verify",
+      Aliases:     []string{"v"},
+      Usage:       "verify a signed manifest",
+      Action:  func(c *cli.Context) error {
+        fmt.Println("verify is not implemented yet")
+        return nil
+      },
+    },
+    {
+      Name:        "run",
+      Aliases:     []string{"v"},
+      Usage:       "starts the HTTP(S) server",
+      Action:  func(c *cli.Context) error {
+        fmt.Println("run is not implemented yet")
+        return nil
+      },
+    },
   }
 
   err := app.Run(os.Args)
+
   if err != nil {
     os.Exit(1)
   }
