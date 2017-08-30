943816efea142d7f4f5c47f9b5cdee203f088543
antoniaklja <antoniaklja@gmail.com>
2017-08-30 16:46:40 +0200

    #1 Initial version of RSA sign/verify

--- generate/generate_test.go
+++ generate/generate_test.go
@@ -7,9 +7,21 @@ import (
 
 func TestGenerateRSAKeyPair(t *testing.T) {
 	// when
-	GenerateKeyPair()
+	privateKey, publicKey, err := GenerateKeyPair()
 
 	// then
+	if err != nil {
+		t.Error("Cannot generate RSA keypair")
+	}
+
+	if privateKey == nil {
+		t.Error("Private Key doesn't exist")
+	}
+
+	if publicKey == nil {
+		t.Error("Public Key doesn't exist")
+	}
+
 	if _, err := os.Stat(PrivateKeyFile); os.IsNotExist(err) {
 		t.Error("Private Key doesn't exist")
 	} else {
@@ -21,5 +33,6 @@ func TestGenerateRSAKeyPair(t *testing.T) {
 	} else {
 		os.Remove(PublicKeyFile)
 	}
-	// FIXME @antoniaklja add more versatile tests
+
+	// TODO @antoniaklja add private and public keys integrity check
 }
