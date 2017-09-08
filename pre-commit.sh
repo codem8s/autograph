eadbd8e28600728bf39e3d9c210b1b621e5e114b
Paweł Prażak <pawel.prazak@gmail.com>
2017-09-08 23:19:25 +0200

    Update pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,30 +2,45 @@
 
 #!/bin/sh
 
+echo
+echo Running gofmt
+echo
 GOFMT_FILES=$(gofmt -l .)
 if [ -n "${GOFMT_FILES}" ]; then
   printf >&2 'gofmt failed for the following files:\n%s\n\nplease run "gofmt -w ." on your changes before committing.\n' "${GOFMT_FILES}"
   exit 1
 fi
 
+echo
+echo Running golint
+echo
 GOLINT_ERRORS=$(golint $(find . -type d -not -path "./vendor/*") | grep -v "Id should be")
 if [ -n "${GOLINT_ERRORS}" ]; then
   printf >&2 'golint failed for the following reasons:\n%s\n\nplease run "golint ./..." on your changes before committing.\n' "${GOLINT_ERRORS}"
   exit 1
 fi
 
+echo
+echo Running goimports
+echo
 GOIMPORTS_FILES=$(goimports -l $(find . -type f -name '*.go' -not -path "./vendor/*"))
 if [ -n "${GOIMPORTS_FILES}" ]; then
   printf >&2 'goimports failed for the following files:\n%s\n\nplease run "goimports -w ." on your changes before committing.\n' "${GOIMPORTS_FILES}"
   exit 1
 fi
 
+echo
+echo Running go vet
+echo
 GOVET_ERRORS=$(go tool vet *.go 2>&1)
 if [ -n "${GOVET_ERRORS}" ]; then
   printf >&2 'go vet failed for the following reasons:\n%s\n\nplease run "go tool vet *.go" on your changes before committing.\n' "${GOVET_ERRORS}"
   exit 1
 fi
 
+echo
+echo Running go dep
+echo
 DEP_ERRORS=$(dep status)
 if [ $? -ne 0 ]; then
   printf >&2 'dep status failed for the following reasons:\n%s\n\nplease run "dep ensure" on your changes before committing.\n' "${DEP_ERRORS}"
