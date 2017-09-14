ed168ac04842dc2fa29be24555c3f55bd7d16e05
Paweł Prażak <pawel.prazak@gmail.com>
2017-09-14 16:16:03 +0200

    Update CI code

--- release.sh
+++ release.sh
@@ -4,14 +4,14 @@ export TAG=${VERSION}-${COMMIT}-${TRAVIS_BRANCH}-go${TRAVIS_GO_VERSION}
 echo REPO=${REPO}, TAG=${TAG}, COMMIT=${COMMIT}, VERSION=${VERSION}, TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}, TRAVIS_GO_VERSION=${TRAVIS_GO_VERSION}
 
 docker build -t ${REPO}:${COMMIT} .
-docker tag ${REPO}:${COMMIT} quay.io/${REPO}:COMMIT
+docker tag ${REPO}:${COMMIT} quay.io/${REPO}:${COMMIT}
 docker tag ${REPO}:${COMMIT} quay.io/${REPO}:${TAG}
 docker tag ${REPO}:${COMMIT} quay.io/${REPO}:travis-${TRAVIS_BUILD_NUMBER}
 
 docker images
 
 docker login -u="$QUAY_USER" -p="$QUAY_PASS" quay.io
-docker push quay.io/${REPO}:COMMIT
+docker push quay.io/${REPO}:${COMMIT}
 docker push quay.io/${REPO}:${TAG}
 docker push quay.io/${REPO}:travis-${TRAVIS_BUILD_NUMBER}
 
