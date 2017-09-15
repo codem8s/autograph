2eabc7778aa7bf43f5416f941f6f9ba1234502ee
Paweł Prażak <pawel.prazak@gmail.com>
2017-09-15 19:44:35 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
-export TAG=${VERSION}-${COMMIT}-${TRAVIS_BRANCH}-go${TRAVIS_GO_VERSION}
-echo REPO=${REPO}, TAG=${TAG}, COMMIT=${COMMIT}, VERSION=${VERSION}, TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}, TRAVIS_GO_VERSION=${TRAVIS_GO_VERSION}
+export BUILD_TAG="travis-$TRAVIS_BUILD_NUMBER-$TRAVIS_BRANCH-$COMMIT-go$TRAVIS_GO_VERSION"
+
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_GO_VERSION}" == "1.8" ]; then
+  export LATEST_TAG="latest"
+  export VERSION_TAG="$VERSION"
+else
+  export LATEST_TAG="latest-$TRAVIS_GO_VERSION"
+  export VERSION_TAG="$COMMIT-$TRAVIS_GO_VERSION"
+fi
+
+echo "REPO=$REPO, COMMIT=$COMMIT, BUILD_TAG=$BUILD_TAG, VERSION_TAG=$VERSION_TAG, LATEST=$LATEST_TAG"
 
 docker build -t ${REPO}:${COMMIT} .
-docker tag ${REPO}:${COMMIT} quay.io/${REPO}:${COMMIT}
-docker tag ${REPO}:${COMMIT} quay.io/${REPO}:${TAG}
-docker tag ${REPO}:${COMMIT} quay.io/${REPO}:travis-${TRAVIS_BUILD_NUMBER}
+docker tag ${REPO}:${COMMIT} quay.io/${REPO}:${VERSION_TAG}
+docker tag ${REPO}:${COMMIT} quay.io/${REPO}:${BUILD_TAG}
+docker tag ${REPO}:${COMMIT} quay.io/${REPO}:${LATEST_TAG}
 
 docker images
 
 docker login -u="$QUAY_USER" -p="$QUAY_PASS" quay.io
-docker push quay.io/${REPO}:${COMMIT}
-docker push quay.io/${REPO}:${TAG}
-docker push quay.io/${REPO}:travis-${TRAVIS_BUILD_NUMBER}
-
-if [ "${TRAVIS_BRANCH}" == "master" ] && [ "${TRAVIS_GO_VERSION}" == "1.8" ]; then
-  docker tag ${REPO}:${COMMIT} quay.io/${REPO}:latest
-  docker push quay.io/${REPO}:latest
-fi
+docker push quay.io/${REPO}:${VERSION_TAG}
+docker push quay.io/${REPO}:${BUILD_TAG}
+docker push quay.io/${REPO}:${LATEST_TAG}
