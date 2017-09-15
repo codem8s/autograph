801add272bc5b058dda7f8a14889d4ebe279c651
Paweł Prażak <pawel.prazak@gmail.com>
2017-09-15 13:07:36 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
 set -e
-echo "" > coverage.txt
+
+option=$1
+
+if [[ ${option} == "with-coverage" ]]; then
+    echo "Running tests with code coverage"
+    has_coverage=true
+fi
 
 for d in $(go list ./... | grep -v vendor); do
-    echo "Running tests with coverage for $d"
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ "$has_coverage" = "true" ]; then
+        echo "Running tests with coverage for $d"
+        go test -race -coverprofile=profile.out -covermode=atomic ${d}
+    else
+        echo "Running tests for $d"
+        go test -timeout 3m ${d}
+    fi
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
 done
 
-bash <(curl -s https://codecov.io/bash)
+if [ "$has_coverage" = "true" ]; then
+    bash <(curl -s https://codecov.io/bash)
+fi
